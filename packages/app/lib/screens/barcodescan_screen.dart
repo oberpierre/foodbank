12044a64265d5884ad80d7d2c5f09e9e05f00abe
Pierre Obermaier <pierre.obermaier@googlemail.com>
2023-02-17 19:40:58 +0800

    F!! Changing to mobile_scanner

--- packages/app/lib/screens/barcodescan_screen.dart
+++ packages/app/lib/screens/barcodescan_screen.dart
@@ -1,6 +1,7 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 
-import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class BarcodescanScreen extends StatefulWidget {
   const BarcodescanScreen({super.key, required this.title});
@@ -37,46 +38,20 @@ class _BarcodeScanState extends State<BarcodescanScreen> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              _barcode,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: MobileScanner(
+        // fit: BoxFit.contain,
+        controller: MobileScannerController(
+          facing: CameraFacing.back,
+          torchEnabled: false,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var barcodeRes = await BarcodeScanner.scan();
-          if (barcodeRes.type == ResultType.Barcode) {
-            _barcode = barcodeRes.rawContent;
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
           }
         },
-        tooltip: 'Scan',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
