aad279608a326e1d9a1c4d0ccf2ef701ff0cc42f
Pierre Obermaier <pierre.obermaier@googlemail.com>
2023-02-18 16:47:20 +0800

    F Introducing initial screens for flow

--- packages/app/lib/screens/barcodescan_screen.dart
+++ packages/app/lib/screens/barcodescan_screen.dart
@@ -1,28 +1,10 @@
-import 'dart:typed_data';
+import 'package:app/screens/detail_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class BarcodescanScreen extends StatefulWidget {
-  const BarcodescanScreen({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<BarcodescanScreen> createState() => _BarcodeScanState();
-}
-
-class _BarcodeScanState extends State<BarcodescanScreen> {
-  String _barcode = '';
+class BarcodescanScreen extends StatelessWidget {
+  const BarcodescanScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +16,9 @@ class _BarcodeScanState extends State<BarcodescanScreen> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          ),
       body: MobileScanner(
         // fit: BoxFit.contain,
         controller: MobileScannerController(
@@ -46,9 +27,12 @@ class _BarcodeScanState extends State<BarcodescanScreen> {
         ),
         onDetect: (capture) {
           final List<Barcode> barcodes = capture.barcodes;
-          final Uint8List? image = capture.image;
-          for (final barcode in barcodes) {
-            debugPrint('Barcode found! ${barcode.rawValue}');
+          String? barcode = barcodes.elementAt(0).rawValue;
+          if (barcode != null) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailScreen(id: barcode)));
           }
         },
       ),
